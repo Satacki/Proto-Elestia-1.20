56b7078cc9b6d651073fb16fa0a20795f5499c0e
Thomas Maxime <m.thomas@student.isartdigital.com>
2025-02-16 02:12:01 +0100

    Idofront armor and gui almost done (  (only 1 bug left )

--- src/main/java/net/lykos/protogmt/client/ModKeybinds.java
+++ src/main/java/net/lykos/protogmt/client/ModKeybinds.java
@@ -14,7 +14,7 @@ import net.lykos.protogmt.gui.BondrewdArmorScreen;
 import net.lykos.protogmt.gui.BondrewdArmorScreenHandler;
 import net.lykos.protogmt.items.IdofrontArmorItem;
 
-//I'm starting to lose my mind
+//I'm starting to loose my mind
 
 public class ModKeybinds {
     public static final String KEY_CATEGORY_PROTOMOD = "key.categories.protogmt";
